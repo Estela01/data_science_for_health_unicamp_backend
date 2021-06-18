70e4256e44c2e75e7f0a4f134f7b9e8c12cc6e8d
Estela Oliveira <maristela1010@gmail.com>
2021-06-18 13:18:54 -0300

    Libera ip

--- fastApi.py
+++ fastApi.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://192.168.0.2:3000", "http://192.168.0.2","*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
