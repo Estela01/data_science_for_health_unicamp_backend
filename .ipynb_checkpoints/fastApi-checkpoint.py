beb5fdb80ba9b66a349670ef429d497e66bef0b6
Estela Oliveira <maristela1010@gmail.com>
2021-06-20 01:55:57 -0300

    Adiciona novas rotas

--- .ipynb_checkpoints/fastApi-checkpoint.py
+++ .ipynb_checkpoints/fastApi-checkpoint.py
@@ -5,11 +5,10 @@ from fastapi.responses import JSONResponse
 
 import json
 
-from functions import getAnomaliasbyCidades
+from functions import getAnomaliasbyCidades, getListAnomalias, getAnomaliasAllYears
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -37,12 +36,19 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/getAnomalias/")
-async def read_item(year: int = 2019):
+async def read_item(year: int = 2019, cid10: str = "Teste"):
     
-    if year < 2009 or year > 2019:
+    if year != -1 and (year < 2009 or year > 2019):
        raise UnicornException(name="Não foi encontrado dados para esse ano.", status_code=404)
             
-    try:    
+    try:
+        print(cid10)
+        if year == -1:
+            anomaliasAllYears = getAnomaliasAllYears()
+            result = anomaliasAllYears.to_json(orient = "records")
+            parsed = jsonable_encoder(result)
+            return JSONResponse(content=parsed)
+        
         anomaliasByCidades = getAnomaliasbyCidades(year)
         result = anomaliasByCidades.to_json(orient = "records")
         parsed = jsonable_encoder(result)
@@ -50,7 +56,27 @@ async def read_item(year: int = 2019):
     except:
        raise UnicornException(name="Server Error", status_code= 500)
 
+    
+@app.get("/getListAnomalias/")
+async def read_item():
+    try:    
+        lista = getListAnomalias()
+        result = lista.to_json(orient = "records")
+        parsed = jsonable_encoder(result)
+        return JSONResponse(content=parsed)
+    except:
+       raise UnicornException(name="Server Error", status_code= 500)
+    
+@app.get("/getListYears/")
+async def read_item():
+    try:
+        yearList = [{"id": -1, "label": "Todos"}]
+        for year in range(2009, 2020):
+            yearList.append({"id": year, "label": "{}".format(year)})
+        return JSONResponse(content=yearList)
+    except:
+       raise UnicornException(name="Server Error", status_code= 500)
 
 if __name__ == "__main__":
-    uvicorn.run("fastApi:app", host="127.0.0.1", port=3000, log_level="info") 
-    #uvicorn.run("fastApi:app", host="192.168.0.9", port=3000, log_level="info")
+    #uvicorn.run("fastApi:app", host="127.0.0.1", port=3000, log_level="info") 
+    uvicorn.run("fastApi:app", host="192.168.0.9", port=3000, log_level="info")
--- functions.py
+++ functions.py
@@ -1,16 +1,61 @@
 import pandas as pd
 import numpy as np
 
+cid = pd.read_csv("./CID10.csv")
+dtAll = pd.DataFrame()
+for year in range(2009, 2020):
+    df = pd.read_csv("./sp_{}_clean.csv".format(year))
+    dtAll = dtAll.append(df[['CODANOMAL', 'CODMUNNASC']])
+
+def getAnomaliaDescription(cod):
+    descricao = cid[cid["CID10"] == cod.strip().replace('X', '').replace('D', '') ]
+    return descricao["DESCR"]
+
+def frequency(my_list): 
+    freq = {}
+    dictionary = []
+    for item in np.unique(np.array(my_list)):
+        freq[item] = np.where(np.array(my_list)==item)[0].shape[0]
+        dictionary.append({"cod": item, "quantidade":freq[item], "descricao": getAnomaliaDescription(item)})
+    return dictionary
+
+def anomaliaFetch(arrayAnomalias):
+    completedArray = []
+    for element in arrayAnomalias:
+        anomaliasRaw = element.split('Q')
+        for anomalia in anomaliasRaw:
+            if(len(anomalia) > 0):
+                anomalia = 'Q'+ anomalia
+                completedArray.append(anomalia)
+    anomaliasObjects = frequency(completedArray)
+    return { "anomalias": anomaliasObjects , "total":  len(completedArray) }
+
+
 def getAnomaliasbyCidades(year):
     df = pd.read_csv("./sp_{}_clean.csv".format(year))
-
     cidades = df["CODMUNNASC"].unique()
-
     array_anomal_cidade = []
-
     for cidade in cidades:
-        json = {"id" : cidade, "anomalias": df[df["CODMUNNASC"] == cidade ]["CODANOMAL"].to_numpy().tolist() }
-
+        json =   {"id" : cidade}
+        arrayAnomalias = df[df["CODMUNNASC"] == cidade ]["CODANOMAL"].to_numpy()
+        infos = anomaliaFetch(arrayAnomalias)
+        json.update(infos)
         array_anomal_cidade.append(json)
-    
     return pd.DataFrame(array_anomal_cidade)
+
+def getAnomaliasAllYears():
+    cidades = dtAll["CODMUNNASC"].unique()
+    array_anomal_cidade = []
+    for cidade in cidades:
+        json =   {"id" : cidade}
+        arrayAnomalias = dtAll[dtAll["CODMUNNASC"] == cidade ]["CODANOMAL"].to_numpy()
+        infos = anomaliaFetch(arrayAnomalias)
+        json.update(infos)
+        array_anomal_cidade.append(json)
+    return pd.DataFrame(array_anomal_cidade)
+
+
+def getListAnomalias():
+    cidAnomalias =  cid[cid["CID10"].str.contains('Q')]
+    cidAnomalias = cidAnomalias[["CID10","DESCR"]]
+    return pd.DataFrame(cidAnomalias)
